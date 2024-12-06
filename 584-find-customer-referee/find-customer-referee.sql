8208c9da0909a5cf883e3434feb6fa8ec4ce8a61
Jaffulee <167854664+Jaffulee@users.noreply.github.com>
2024-12-06 15:47:31 +0000

    Time: 487 ms (88.53%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,4 @@
 /* Write your T-SQL query statement below */
 SELECT name
 FROM Customer
-WHERE  (referee_id <> '2') OR (referee_id IS NULL)
+WHERE  (Customer.referee_id <> '2') OR (Customer.referee_id IS NULL)
