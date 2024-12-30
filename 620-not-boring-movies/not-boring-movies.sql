e50df7fc65e9a4c0edadc22cabf14db2ca1473b9
Jaffulee <167854664+Jaffulee@users.noreply.github.com>
2024-12-30 10:42:32 +0000

    Time: 434 ms (26.90%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,8 +1,5 @@
 /* Write your T-SQL query statement below */
-SELECT C.id
-        ,C.movie
-        ,C.description
-        ,C.rating
-FROM Cinema AS C
-WHERE NOT RIGHT(C.id,1) IN (0,2,4,6,8) AND C.description <> 'boring'
-ORDER BY C.rating DESC
+SELECT *
+FROM Cinema
+WHERE NOT RIGHT(Cinema.id,1) IN (0,2,4,6,8) AND Cinema.description <> 'boring'
+ORDER BY Cinema.rating DESC
