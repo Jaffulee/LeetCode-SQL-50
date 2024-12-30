0f73d8767b32eca001cd3e999cc54c45ebba34e9
Jaffulee <167854664+Jaffulee@users.noreply.github.com>
2024-12-30 10:44:19 +0000

    Time: 416 ms (29.96%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 SELECT *
 FROM Cinema
-WHERE NOT RIGHT(Cinema.id,1) IN (0,2,4,6,8) AND Cinema.description <> 'boring'
+WHERE NOT Cinema.id%2 = 0 AND Cinema.description <> 'boring'
 ORDER BY Cinema.rating DESC
