13a9153ec6c593cd0baa32175b7863f3fe4239cb
Jaffulee <167854664+Jaffulee@users.noreply.github.com>
2024-12-30 11:09:04 +0000

    Time: 696 ms (15.79%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 SELECT p.product_id
-        ,ROUND(COALESCE(SUM(CAST(p.price*u.units AS FLOAT)) / SUM(CAST(u.units AS FLOAT)), 0),2) AS average_price
+        ,ROUND(COALESCE(SUM(CAST(p.price*u.units AS FLOAT)) / SUM(CAST(u.units AS FLOAT)), 0),2) AS average_price -- Catch divide by 0, when true then output second value which is 0
 FROM prices as p
 LEFT JOIN UnitsSold as u
 ON p.product_id = u.product_id AND u.purchase_date>=p.start_date AND u.purchase_date<=p.end_date
