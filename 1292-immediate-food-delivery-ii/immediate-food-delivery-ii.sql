203ed0062d00bbb72ca98dcb3729cf077fc0c3f3
Jaffulee <167854664+Jaffulee@users.noreply.github.com>
2024-12-31 12:30:21 +0000

    Time: 4104 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,18 @@
+/* Write your T-SQL query statement below */
+
+
+SELECT
+    ROUND(100.0*(SUM(CASE WHEN Delivery.order_date = Delivery.customer_pref_delivery_date THEN 1 ELSE 0 END)) 
+    / 
+    COUNT(*),2)
+    AS immediate_percentage
+FROM Delivery
+INNER JOIN (
+    SELECT 
+        d.customer_id
+        ,MIN(d.order_date) AS first_order
+    FROM Delivery AS d
+    GROUP BY d.customer_id
+    ) AS m
+ON Delivery.customer_id = m.customer_id
+WHERE Delivery.order_date=m.first_order
